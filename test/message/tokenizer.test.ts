ee0dc19218c05f127bc85a08e2e4b9f2da428efa
kazuya kawaguchi <kawakazu80@gmail.com>
2020-02-17 11:52:13 +0900

    add more tokenizer snapshot testing

--- test/message/tokenizer.test.ts
+++ test/message/tokenizer.test.ts
@@ -13,7 +13,15 @@ test('parse', () => {
     'hi {0} !',
     '{0} {1}　{2}',
     'hi {  -1 } !',
-    '{0}\n{1}\r\n{2}'
+    '{0}\n{1}\r\n{2}',
+    'hi @:name !',
+    'hi @:(hello world) !',
+    'hi @:{name}\n !',
+    'hi @.upper:name !',
+    'hi @:{name} @:{0}!',
+    'no apples | one apple  |  too much apples  ',
+    'no apples |\n one apple  |\n  too much apples  ',
+    '@.lower:(no apples) | {1} apple | {count}　apples'
   ].forEach(p => {
     expect(parse(p)).toMatchSnapshot(JSON.stringify(p))
   })
