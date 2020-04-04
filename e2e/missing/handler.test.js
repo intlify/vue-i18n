5daa1c789fac493fb18490c9423d3a40b9e683cd
kazuya kawaguchi <kawakazu80@gmail.com>
2020-04-05 01:38:25 +0900

    add missing e2e

---
+++ e2e/missing/handler.test.js
@@ -0,0 +1,26 @@
+const { setupWarningConsole } = require('../helper') // eslint-disable-line
+
+;['composable', 'legacy'].forEach(pattern => {
+  describe(`${pattern}`, () => {
+    const warnings = []
+    beforeAll(async () => {
+      setupWarningConsole(page, warnings)
+      await page.goto(
+        `http://localhost:8080/examples/${pattern}/missing/handler.html`
+      )
+    })
+
+    test('warning', () => {
+      // missing handler warning
+      expect(warnings[0]).toEqual(`detect 'message.hello' key missing in 'ja'`)
+      // fallback warning
+      expect(warnings[1]).toEqual(
+        `[vue-i18n] Fall back to translate 'message.hello' key with 'en' locale.`
+      )
+    })
+
+    test('rendering', async () => {
+      await expect(page).toMatch('hello world!')
+    })
+  })
+})
---
+++ e2e/missing/option.test.js
@@ -0,0 +1,24 @@
+const { setupWarningConsole } = require('../helper') // eslint-disable-line
+
+;['composable'].forEach(pattern => {
+  describe(`${pattern}`, () => {
+    const warnings = []
+    beforeAll(async () => {
+      setupWarningConsole(page, warnings)
+      await page.goto(
+        `http://localhost:8080/examples/${pattern}/missing/option.html`
+      )
+    })
+
+    test('warning', () => {
+      // fallback warning only
+      expect(warnings[0]).toEqual(
+        `[vue-i18n] Fall back to translate 'message.hello' key with 'en' locale.`
+      )
+    })
+
+    test('rendering', async () => {
+      await expect(page).toMatch('hello, kazupon!')
+    })
+  })
+})
