27148a68d9a202f5a9b0a0961724dae66057b334
kazuya kawaguchi <kawakazu80@gmail.com>
2020-04-26 00:55:15 +0900

    add literal usage examples (#21)

---
+++ e2e/formatting/literal.test.js
@@ -0,0 +1,18 @@
+;['composable', 'legacy'].forEach(pattern => {
+  describe(`${pattern}`, () => {
+    beforeAll(async () => {
+      await page.goto(
+        `http://localhost:8080/examples/${pattern}/formatting/literal.html`
+      )
+    })
+
+    test('initial rendering', async () => {
+      await expect(page).toMatch('こんにちは、かずぽん！')
+    })
+
+    test('change locale', async () => {
+      await page.select('#app select', 'en')
+      await expect(page).toMatch('Hello, kazupon!')
+    })
+  })
+})
