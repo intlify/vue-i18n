9c8c40962bc218fe8b9cdabdb1137a3c972ffd45
Daniel Abrão <danielpaladar@gmail.com>
2020-10-04 11:40:33 -0300

    Fix `UNEXPECTED_ERROR` message typo (#134)

--- src/errors.ts
+++ src/errors.ts
@@ -41,7 +41,7 @@ export const errorMessages: { [code: number]: string } = {
   [I18nErrorCodes.UNEXPECTED_RETURN_TYPE]: 'Unexpected return type in composer',
   [I18nErrorCodes.INVALID_ARGUMENT]: 'Invalid argument',
   [I18nErrorCodes.NOT_INSLALLED]: 'Need to install with app.use function',
-  [I18nErrorCodes.UNEXPECTED_ERROR]: 'Unexpeced error',
+  [I18nErrorCodes.UNEXPECTED_ERROR]: 'Unexpected error',
   [I18nErrorCodes.NOT_AVAILABLE_IN_LEGACY_MODE]: 'Not available in legacy mode',
   [I18nErrorCodes.REQUIRED_VALUE]: `Required in value: {0}`,
   [I18nErrorCodes.INVALID_VALUE]: `Invalid value`,
