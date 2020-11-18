e47aa7e8229f1ed8c41bbf8356d77d4dd7de1e61
kazuya kawaguchi <kawakazu80@gmail.com>
2020-11-18 18:34:45 +0900

    add jest setup for e2e

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -128,7 +128,7 @@ module.exports = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ['./jest.e2e.setup.js'],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
