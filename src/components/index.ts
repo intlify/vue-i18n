2969c430b7194305ca58c4365e95296300e3c9b1
kazuya kawaguchi <kawakazu80@gmail.com>
2020-12-08 03:18:57 +0900

    remove old codes

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { ComponetI18nScope, BaseFormatProps } from './base'
-export { FormattableProps } from './formatRenderer'
-export { Translation, TranslationProps } from './Translation'
-export { NumberFormat, NumberFormatProps } from './NumberFormat'
-export { DatetimeFormat, DatetimeFormatProps } from './DatetimeFormat'
