0ad8896243fda5c30fe4081b61c23c2d3189424a
kazuya kawaguchi <kawakazu80@gmail.com>
2020-12-09 18:40:00 +0900

    message-compiler: update unit tests

--- packages/message-compiler/test/compiler.test.ts
+++ packages/message-compiler/test/compiler.test.ts
@@ -1,71 +1,38 @@
 /* eslint-disable @typescript-eslint/no-empty-function, no-irregular-whitespace */
 
-test('compiler', () => {
-  expect(true).toBe(true)
-})
-// utils
-/*
-jest.mock('@intlify/shared', () => ({
-  ...jest.requireActual<object>('@intlify/shared'),
-  warn: jest.fn()
-}))
-import { warn } from '@intlify/shared'
-
 import { baseCompile as compile } from '../src/compiler'
 
 test(`@.caml:{'no apples'} | {0} apple | {n}　apples`, () => {
-  const code = compile(`@.caml:{'no apples'} | {0} apple | {n}　apples`)
-  expect(code.toString()).toMatchSnapshot('code')
-})
-
-describe('warnHtmlMessage', () => {
-  test('default', () => {
-    const mockWarn = warn as jest.MockedFunction<typeof warn>
-    mockWarn.mockImplementation(() => {})
-
-    const code = compile('<p>hello</p>')
-    expect(code.toString()).toMatchSnapshot('code')
-    expect(mockWarn).toHaveBeenCalled()
-    expect(mockWarn.mock.calls[0][0]).toEqual(
-      `Detected HTML in '<p>hello</p>' message. Recommend not using HTML messages to avoid XSS.`
-    )
-  })
-
-  test('false', () => {
-    const mockWarn = warn as jest.MockedFunction<typeof warn>
-    mockWarn.mockImplementation(() => {})
-
-    const code = compile('<p>hello</p>', { warnHtmlMessage: false })
-    expect(code.toString()).toMatchSnapshot('code')
-    expect(mockWarn).not.toHaveBeenCalled()
-  })
+  const { code, ast, map } = compile(
+    `@.caml:{'no apples'} | {0} apple | {n}　apples`
+  )
+  expect(code).toMatchSnapshot('code')
+  expect(ast).toMatchSnapshot('ast')
+  expect(map).toMatchSnapshot('map')
 })
 
 describe('edge cases', () => {
   test(` | | | `, () => {
-    const code = compile(` | | | `, {
+    const { code } = compile(` | | | `, {
       onError(error) {
         expect({ ...error, message: error.message }).toMatchSnapshot('error')
       }
     })
-    expect(code.toString()).toMatchSnapshot('code')
+    expect(code).toMatchSnapshot('code')
   })
 
   test(`hi %s !`, () => {
-    const code = compile(`hi %s !`)
-    expect(code.toString()).toMatchSnapshot('code')
+    const { code } = compile(`hi %s !`)
+    expect(code).toMatchSnapshot('code')
   })
 
   test(`%`, () => {
-    const code = compile(`%`)
-    expect(code.toString()).toMatchSnapshot('code')
+    const { code } = compile(`%`)
+    expect(code).toMatchSnapshot('code')
   })
 
   test(`no apples %| one apple % |  too much apples  `, () => {
-    const code = compile(`no apples %| one apple % |  too much apples  `)
-    expect(code.toString()).toMatchSnapshot('code')
+    const { code } = compile(`no apples %| one apple % |  too much apples  `)
+    expect(code).toMatchSnapshot('code')
   })
 })
-*/
-
-/* eslint-enable @typescript-eslint/no-empty-function, no-irregular-whitespace  */
