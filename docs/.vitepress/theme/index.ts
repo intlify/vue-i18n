b6f429ea6d3d675881df92a28086ec5ddf1120c8
kazuya kawaguchi <kawakazu80@gmail.com>
2021-04-13 12:04:15 +0900

    remove not used code

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,4 @@
-import { h, watch } from 'vue'
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/dist/client/theme-default'
 import Layout from './Layout.vue'
 import Sponsor from './components/Sponsor.vue'
