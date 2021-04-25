eb0658db0da13aaa60215058a66b68b611912cb7
PeterAlfredLee <peteralfredlee@gmail.com>
2021-04-25 16:52:19 +0800

    simplify conditional judgment

--- packages/vue-i18n/src/components/Translation.ts
+++ packages/vue-i18n/src/components/Translation.ts
@@ -118,12 +118,9 @@ export const Translation = {
         options
       )
       const assignedAttrs = assign({}, attrs)
-      // prettier-ignore
-      return isString(props.tag)
+      return isString(props.tag) || isObject(props.tag)
         ? h(props.tag, assignedAttrs, children)
-        : isObject(props.tag)
-          ? h(props.tag, assignedAttrs, children)
-          : h(Fragment, assignedAttrs, children)
+        : h(Fragment, assignedAttrs, children)
     }
   }
 }
