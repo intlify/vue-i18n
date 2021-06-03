df37529bf4c91cfc82301abe42c8a2995f645145
kazuya kawaguchi <kawakazu80@gmail.com>
2021-06-04 04:24:24 +0900

    add tsx / jsx exammple
    closes #517

---
+++ examples/tsx/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<script lang="tsx">
+import { defineComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+  setup({ msg }) {
+    const { t } = useI18n({
+      useScope: 'global',
+      inheritLocale: true
+    })
+    return () => (
+      <>
+        <h1>{msg}</h1>
+        <p>v-t: <span v-t="hello"></span></p>
+        <i18n-t keypath="term" tag="label" for="tos">
+          <a href="https://vue-i18n.intlify.dev" target="_blank">{ t('tos') }</a>
+        </i18n-t>
+      </>
+    )
+  }
+})
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
---
+++ examples/tsx/src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+import App from './App.vue'
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'en',
+  messages: {
+    en: {
+      hello: 'Hello world',
+      tos: 'vue-i18n-next docs',
+      term: 'access to {0}'
+    }
+  }
+})
+
+createApp(App).use(i18n).mount('#app')
