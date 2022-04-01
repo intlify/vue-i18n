590a13bfca8cafd8e7378280bf418cab6bb0765c
kazuya kawaguchi <kawakazu80@gmail.com>
2022-04-01 14:33:10 +0900

    fix(components): strictly slot rendering
    related #968

--- packages/vue-i18n-core/src/components/formatRenderer.ts
+++ packages/vue-i18n-core/src/components/formatRenderer.ts
@@ -5,6 +5,7 @@ import { isString, isObject, isArray, assign } from '@intlify/shared'
 import type {
   RenderFunction,
   SetupContext,
+  VNode,
   VNodeChild,
   VNodeArrayChildren
 } from 'vue'
@@ -39,6 +40,10 @@ type FormatPartReturn = Intl.NumberFormatPart | Intl.DateTimeFormatPart
 type FormatOverrideOptions =
   | Intl.NumberFormatOptions
   | Intl.DateTimeFormatOptions
+
+function isVNode(target: unknown, slot: unknown): target is VNode[] {
+  return isArray(target) && !slot
+}
 
 export function renderFormatter<
   Props extends FormattableProps<Value, Format>,
@@ -83,9 +88,13 @@ export function renderFormatter<
     if (isArray(parts)) {
       children = parts.map((part, index) => {
         const slot = slots[part.type]
-        return slot
+        const node = slot
           ? slot({ [part.type]: part.value, index, parts })
           : [part.value]
+        if (isVNode(node, slot)) {
+          node[0].key = `${part.type}-${index}`
+        }
+        return node
       })
     } else if (isString(parts)) {
       children = [parts]
