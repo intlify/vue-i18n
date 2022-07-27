7a38f866237ed6525b8c175bea65921376100ab0
kazuya kawaguchi <kawakazu80@gmail.com>
2022-07-27 21:02:37 +0900

    feat: support SSR for v-t (#1087)

--- packages/vue-i18n-core/test/ssr.test.ts
+++ packages/vue-i18n-core/test/ssr.test.ts
@@ -1,4 +1,12 @@
-import { h, defineComponent, resolveComponent, createSSRApp } from 'vue'
+import {
+  h,
+  ref,
+  defineComponent,
+  resolveComponent,
+  resolveDirective,
+  withDirectives,
+  createSSRApp
+} from 'vue'
 import { renderToString } from '@vue/server-renderer'
 import {
   compileToFunction,
@@ -84,4 +92,114 @@ test('component: i18n-t', async () => {
   app.use(i18n)
 
   expect(await renderToString(app)).toMatch(`<p>こんにちは！</p>`)
+})
+
+describe('custom directive: v-t', () => {
+  test('basic', async () => {
+    const i18n = createI18n({
+      legacy: false,
+      locale: 'en',
+      messages: {
+        en: {
+          hello: 'hello!'
+        }
+      }
+    })
+
+    const App = defineComponent({
+      setup() {
+        // <p v-t="'hello'"></p>
+        const t = resolveDirective('t')
+        return () => {
+          return withDirectives(h('p'), [[t!, 'hello']])
+        }
+      }
+    })
+    const app = createSSRApp(App)
+    app.use(i18n)
+
+    expect(await renderToString(app)).toEqual('<p>hello!</p>')
+  })
+
+  test('binding', async () => {
+    const i18n = createI18n({
+      locale: 'en',
+      messages: {
+        en: {
+          hello: 'hello!'
+        }
+      }
+    })
+
+    const App = defineComponent({
+      setup() {
+        // <p v-t="msg"></p>
+        const msg = ref('hello')
+        const t = resolveDirective('t')
+        return () => {
+          return withDirectives(h('p'), [[t!, msg.value]])
+        }
+      }
+    })
+    const app = createSSRApp(App)
+    app.use(i18n)
+
+    expect(await renderToString(app)).toEqual('<p>hello!</p>')
+  })
+
+  test('object literal', async () => {
+    const i18n = createI18n({
+      locale: 'en',
+      messages: {
+        en: {
+          hello: 'hello, {name}!'
+        },
+        ja: {
+          hello: 'こんにちは、{name}！'
+        }
+      }
+    })
+
+    const App = defineComponent({
+      setup() {
+        // <p v-t="{ path: 'hello', locale: 'ja', args: { name: name.value } }"></p>
+        const name = ref('kazupon')
+        const t = resolveDirective('t')
+        return () => {
+          return withDirectives(h('p'), [
+            [t!, { path: 'hello', locale: 'ja', args: { name: name.value } }]
+          ])
+        }
+      }
+    })
+    const app = createSSRApp(App)
+    app.use(i18n)
+
+    expect(await renderToString(app)).toEqual('<p>こんにちは、kazupon！</p>')
+  })
+
+  test('plural', async () => {
+    const i18n = createI18n({
+      locale: 'en',
+      messages: {
+        en: {
+          banana: 'no bananas | {n} banana | {n} bananas'
+        }
+      }
+    })
+
+    const App = defineComponent({
+      setup() {
+        // <p v-t="{ path: 'banana', choice: 2 }"></p>
+        const t = resolveDirective('t')
+        return () => {
+          return withDirectives(h('p'), [[t!, { path: 'banana', choice: 2 }]])
+        }
+      }
+    })
+    const app = createSSRApp(App)
+    app.use(i18n)
+
+    expect(await renderToString(app)).toEqual('<p>2 bananas</p>')
+  })
 })
