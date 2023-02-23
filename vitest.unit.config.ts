4660d290394a5c56276ec3b91529eaed5bb3d069
kazuya kawaguchi <kawakazu80@gmail.com>
2023-02-24 01:00:20 +0900

    fix

--- vitest.unit.config.ts
+++ vitest.unit.config.ts
@@ -5,6 +5,7 @@ export default {
   ...config,
   test: {
     ...config.test,
+    environmentMatchGlobs: [['packages/vue-i18n-core/**', 'jsdom']],
     exclude: [...configDefaults.exclude, '**/e2e/**']
   }
 } as UserConfig
