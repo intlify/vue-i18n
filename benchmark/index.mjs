73114d929739945cece95d7416ac76a6db343148
kazuya kawaguchi <kawakazu80@gmail.com>
2023-06-26 19:13:16 +0900

    format

--- benchmark/index.mjs
+++ benchmark/index.mjs
@@ -3,14 +3,14 @@ import { spawn } from 'child_process'
 function run(pattner) {
   return new Promise((resolve, reject) => {
     const child = spawn('node', [`./benchmark/${pattner}.mjs`], {
-      stdio: 'inherit',
+      stdio: 'inherit'
     })
 
-    child.once('error', (err) => {
+    child.once('error', err => {
       reject(err)
     })
 
-    child.once('exit', (code) => {
+    child.once('exit', code => {
       if (code !== 0) {
         reject(new Error(`exit with code ${code}`))
       } else {
@@ -22,7 +22,13 @@ function run(pattner) {
 
 ;(async () => {
   try {
-    for (const p of ['compile', 'simple', 'simple-jit', 'complex', 'complex-jit']) {
+    for (const p of [
+      'compile',
+      'simple',
+      'simple-jit',
+      'complex',
+      'complex-jit'
+    ]) {
       await run(p)
       console.log()
     }
