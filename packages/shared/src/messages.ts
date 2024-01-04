07327ec269297a64cd9aba1e7342bfcc000d1fdb
Pavel Kuzmin <Virus191288@gmail.com>
2024-01-04 18:24:08 +0500

    more fast version

--- packages/shared/src/messages.ts
+++ packages/shared/src/messages.ts
@@ -1,27 +1,38 @@
 import { hasOwn, isArray, isObject } from './utils'
 
+const isNotObjectOrIsArray = (val: unknown) => !isObject(val) || isArray(val)
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
-export function deepCopy(src: any, des: any): any {
-  if (isArray(src) && isArray(des)) {
-    for (let i = 0; i < src.length; i++) {
-      if (isObject(src[i])) {
-        des[i] = deepCopy(src[i], des[i] || (isArray(src[i]) ? [] : {}))
-      } else {
-        des[i] = src[i]
-      }
-    }
-  } else if (isObject(src) && isObject(des)) {
-    for (const key in src) {
-      if (hasOwn(src, key)) {
-        if (isObject(src[key])) {
-          des[key] = deepCopy(src[key], des[key] || {})
-        } else {
-          des[key] = src[key]
-        }
-      }
-    }
-  } else {
+export function deepCopy(src: any, des: any): void {
+  // src and des should both be objects, and none of them can be a array
+  if (isNotObjectOrIsArray(src) || isNotObjectOrIsArray(des)) {
     throw new Error('Invalid value')
   }
-  return des
+
+  for (const key in src) {
+    if (hasOwn(src, key)) {
+      if (isArray(src[key])) {
+        // replace with src[key] when:
+        // src[key] is not an array
+        for (let i = 0; i < src[key].length; i++) {
+          if (!des[key]) des[key] = []
+          if (isObject(src[key][i])) {
+            des[key][i] = Object.assign(
+              src[key] ? src[key][i] || {} : {},
+              des[key] ? des[key][i] ?? {} : {}
+            )
+          } else {
+            des[key][i] = src[key][i]
+          }
+        }
+      } else if (isNotObjectOrIsArray(src[key])) {
+        // replace with src[key] when:
+        // src[key] is not an object, or
+        des[key] = src[key]
+      } else {
+        if (!des[key]) des[key] = {}
+        // src[key] and des[key] are both objects, merge them
+        deepCopy(src[key], des[key])
+      }
+    }
+  }
 }
