a88149d044c92689af083f7399e952a3a0e79642
Pavel Kuzmin <Virus191288@gmail.com>
2024-01-04 16:23:37 +0500

    fix export

--- packages/shared/src/messages.ts
+++ packages/shared/src/messages.ts
@@ -1,27 +1,27 @@
 import { hasOwn, isArray, isObject } from './utils'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
-function deepCopy(src: any, des: any): any {
+export function deepCopy(src: any, des: any): any {
   if (isArray(src) && isArray(des)) {
     for (let i = 0; i < src.length; i++) {
       if (isObject(src[i])) {
-        des[i] = deepCopy(src[i], des[i] || (isArray(src[i]) ? [] : {}));
+        des[i] = deepCopy(src[i], des[i] || (isArray(src[i]) ? [] : {}))
       } else {
-        des[i] = src[i];
+        des[i] = src[i]
       }
     }
   } else if (isObject(src) && isObject(des)) {
     for (const key in src) {
       if (hasOwn(src, key)) {
         if (isObject(src[key])) {
-          des[key] = deepCopy(src[key], des[key] || {});
+          des[key] = deepCopy(src[key], des[key] || {})
         } else {
-          des[key] = src[key];
+          des[key] = src[key]
         }
       }
     }
   } else {
-    throw new Error("Invalid value");
+    throw new Error('Invalid value')
   }
-  return des;
+  return des
 }
