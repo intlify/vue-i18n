dad192b344ce04988ffdc9b431aa2ca67603cc02
supercpq <supercpq@outlook.com>
2024-07-17 22:30:37 +0800

    feat: supports selecting the overwrite order of merge messages

--- packages/shared/src/messages.ts
+++ packages/shared/src/messages.ts
@@ -2,7 +2,7 @@ import { isArray, isObject } from './utils'
 
 const isNotObjectOrIsArray = (val: unknown) => !isObject(val) || isArray(val)
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function deepCopy(src: any, des: any): void {
+export function deepCopy(src: any, des: any, isRetainExistMessage?: boolean): void {
   // src and des should both be objects, and none of them can be a array
   if (isNotObjectOrIsArray(src) || isNotObjectOrIsArray(des)) {
     throw new Error('Invalid value')
@@ -17,7 +17,9 @@ export function deepCopy(src: any, des: any): void {
         // replace with src[key] when:
         // src[key] or des[key] is not an object, or
         // src[key] or des[key] is an array
-        des[key] = src[key]
+        // if you need to keep the existing message on the i18n instance when merging, 
+        // you do not need to perform the following replacement
+        !isRetainExistMessage && (des[key] = src[key])
       } else {
         // src[key] and des[key] are both objects, merge them
         stack.push({ src: src[key], des: des[key] })
