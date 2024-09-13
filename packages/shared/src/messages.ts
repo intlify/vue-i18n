05b235338c9ae1f5df4fb559d7d95cb251259a6d
Bobbie Goede <bobbiegoede@gmail.com>
2024-09-13 12:40:39 +0200

    fix: `deepCopy` should never merge arrays

--- packages/shared/src/messages.ts
+++ packages/shared/src/messages.ts
@@ -12,6 +12,7 @@ export function deepCopy(src: any, des: any): void {
   while (stack.length) {
     const { src, des } = stack.pop()!
 
+    // using `Object.keys` which skips prototype properties
     Object.keys(src).forEach(key => {
       // if src[key] is an object/array, set des[key]
       // to empty object/array to prevent setting by reference
@@ -19,14 +20,14 @@ export function deepCopy(src: any, des: any): void {
         des[key] = Array.isArray(src[key]) ? [] : {}
       }
 
-      if (isObject(des[key]) && isObject(src[key])) {
-        // src[key] and des[key] are both objects, merge them
-        stack.push({ src: src[key], des: des[key] })
-      } else {
+      if (isNotObjectOrIsArray(des[key]) || isNotObjectOrIsArray(src[key])) {
         // replace with src[key] when:
         // src[key] or des[key] is not an object, or
         // src[key] or des[key] is an array
         des[key] = src[key]
+      } else {
+        // src[key] and des[key] are both objects, merge them
+        stack.push({ src: src[key], des: des[key] })
       }
     })
   }
