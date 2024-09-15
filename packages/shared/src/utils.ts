28a83baeaf0b99f8246bc64d7312862ed6e457a0
Bobbie Goede <bobbiegoede@gmail.com>
2024-09-15 12:28:01 +0200

    fix: revert #1379 (#1953)

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -141,11 +141,8 @@ export const objectToString = Object.prototype.toString
 export const toTypeString = (value: unknown): string =>
   objectToString.call(value)
 
-export const isPlainObject = (val: unknown): val is object => {
-  if (!isObject(val)) return false
-  const proto = Object.getPrototypeOf(val)
-  return proto === null || proto.constructor === Object
-}
+export const isPlainObject = (val: unknown): val is object =>
+  toTypeString(val) === '[object Object]'
 
 // for converting list and named values to displayed strings.
 export const toDisplayString = (val: unknown): string => {
