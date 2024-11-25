ac10bc303f1fb73bf93d0c5c30a185583478b6b7
kazuya kawaguchi <kawakazu80@gmail.com>
2024-11-25 17:09:47 +0900

    chore: fix not e2e in local on v9 (#2027)

--- scripts/vitest.globalSetup.ts
+++ scripts/vitest.globalSetup.ts
@@ -1,14 +1,19 @@
 import { fileURLToPath } from 'node:url'
 import { URL } from 'node:url'
 
-// @ts-ignore
-import serveStatic from 'serve-static'
 import { listen } from 'listhen'
+import handler from 'serve-handler'
 
 const __dirname = fileURLToPath(new URL('..', import.meta.url))
 
 export async function setup() {
-  const listener = await listen(serveStatic(__dirname), { port: 8080 })
+  const listener = await listen(
+    (req, res) => {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      handler(req, res, { public: __dirname })
+    },
+    { port: 8080 }
+  )
   return async () => {
     await listener.close()
   }
