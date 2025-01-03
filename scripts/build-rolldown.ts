e91adf1abe8964dbd92a48df28fa3aa5782226c6
kazuya kawaguchi <kawakazu80@gmail.com>
2025-01-04 02:13:59 +0900

    chore: swtich to rolldown bundling

---
+++ scripts/build-rolldown.ts
@@ -0,0 +1,335 @@
+/*
+Produces production builds and stitches together d.ts files.
+
+To specify the package to build, simply pass its name and the desired build
+formats to output (defaults to `buildOptions.formats` specified in that package,
+or "esm,cjs"):
+
+```
+# name supports fuzzy match. will build all packages with name containing "core-base":
+pnpm build core-base
+
+# specify the format to output
+pnpm build core --formats cjs
+```
+*/
+
+import { spawnSync } from 'node:child_process'
+import { existsSync, promises as fs } from 'node:fs'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { parseArgs } from 'node:util'
+import { brotliCompressSync, gzipSync } from 'node:zlib'
+import pc from 'picocolors'
+import { rolldown } from 'rolldown'
+import { createConfigsForPackage } from './rolldown'
+import {
+  targets as allTargets,
+  checkSizeDistFiles,
+  displaySize,
+  fuzzyMatchTarget,
+  readJson
+} from './utils'
+
+import type { OutputOptions } from 'rolldown'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const commit = spawnSync('git', ['rev-parse', '--short=7', 'HEAD'])
+  .stdout.toString()
+  .trim()
+
+const { values, positionals: targets } = parseArgs({
+  allowPositionals: true,
+  options: {
+    formats: {
+      type: 'string',
+      short: 'f'
+    },
+    devOnly: {
+      type: 'boolean',
+      short: 'd'
+    },
+    prodOnly: {
+      type: 'boolean',
+      short: 'p'
+    },
+    withTypes: {
+      type: 'boolean',
+      short: 't'
+    },
+    sourceMap: {
+      type: 'boolean',
+      short: 's'
+    },
+    release: {
+      type: 'boolean'
+    },
+    all: {
+      type: 'boolean',
+      short: 'a'
+    },
+    size: {
+      type: 'boolean'
+    }
+  }
+})
+
+const {
+  formats: rawFormats,
+  all: buildAllMatching,
+  devOnly,
+  prodOnly,
+  withTypes: buildTypes,
+  sourceMap,
+  release: isRelease,
+  size
+} = values
+
+const formats = rawFormats?.split(',')
+const sizeDir = path.resolve(__dirname, '../temp/size')
+
+async function main() {
+  await run()
+
+  async function run() {
+    if (size) {
+      await fs.mkdir(sizeDir, { recursive: true })
+    }
+
+    // const rtsCachePath = path.resolve(__dirname, './node_modules/.rts2_cache')
+    // if (isRelease && existsSync(rtsCachePath)) {
+    //   // remove build cache for release builds to avoid outdated enum values
+    //   await fs.rm(rtsCachePath, { recursive: true })
+    // }
+
+    const resolvedTargets = targets.length
+      ? await fuzzyMatchTarget(targets, buildAllMatching)
+      : await allTargets()
+    await buildAll(resolvedTargets)
+
+    if (size) {
+      await checkAllSizes(resolvedTargets)
+    }
+
+    if (buildTypes) {
+      // TODO: build types
+    }
+  }
+
+  async function buildAll(targets: string[]) {
+    const start = performance.now()
+    const all = []
+    let count = 0
+    for (const target of targets) {
+      const configs = await createConfigsForTarget(target)
+      if (configs) {
+        all.push(
+          Promise.all(
+            configs.map(c =>
+              rolldown(c).then(bundle => {
+                return bundle.write(c.output as OutputOptions).then(() => {
+                  // @ts-expect-error
+                  return path.join('packages', target, c.output.file)
+                })
+              })
+            )
+          ).then(files => {
+            files.forEach(f => {
+              count++
+              console.log(pc.gray('built: ') + pc.green(f))
+            })
+          })
+        )
+      }
+    }
+    await Promise.all(all)
+    // await runParallel(os.cpus().length, targets, build)
+    console.log(
+      `\n${count} files built in ${(performance.now() - start).toFixed(2)}ms.`
+    )
+  }
+
+  async function createConfigsForTarget(target: string) {
+    const pkgDir = path.resolve(__dirname, `../packages/${target}`)
+    const pkg = await readJson(`${pkgDir}/package.json`)
+
+    // only build published packages for release
+    if (isRelease && pkg.private) {
+      return
+    }
+
+    // if building a specific format, do not remove dist.
+    if (!formats && existsSync(`${pkgDir}/dist`)) {
+      await fs.rm(`${pkgDir}/dist`, { recursive: true })
+    }
+
+    return createConfigsForPackage({
+      target,
+      commit,
+      formats,
+      prodOnly,
+      sourceMap
+    })
+    // const env =
+    //   (pkg.buildOptions && pkg.buildOptions.env) ||
+    //   (devOnly ? 'development' : 'production')
+    // await execa(
+    //   'rollup',
+    //   [
+    //     '-c',
+    //     '--environment',
+    //     [
+    //       `COMMIT:${commit}`,
+    //       `NODE_ENV:${env}`,
+    //       `TARGET:${target}`,
+    //       formats ? `FORMATS:${formats}` : ``,
+    //       buildTypes ? `TYPES:true` : ``,
+    //       prodOnly ? `PROD_ONLY:true` : ``,
+    //       sourceMap ? `SOURCE_MAP:true` : ``
+    //     ]
+    //       .filter(Boolean)
+    //       .join(',')
+    //   ],
+    //   { stdio: 'inherit' }
+    // )
+
+    /*
+    if (buildTypes && pkg.types) {
+      console.log()
+      console.log(
+        pc.bold(pc.yellow(`Rolling up type definitions for ${target}...`))
+      )
+
+      // build types
+      const extractorConfigPath = path.resolve(pkgDir, `api-extractor.json`)
+      const extractorConfig =
+        ExtractorConfig.loadFileAndPrepare(extractorConfigPath)
+      const extractorResult = Extractor.invoke(extractorConfig, {
+        localBuild: true,
+        showVerboseMessages: true
+      })
+
+      if (extractorResult.succeeded) {
+        // concat additional d.ts to rolled-up dts
+        const typesDir = path.resolve(pkgDir, 'types')
+        if (existsSync(typesDir)) {
+          const dtsPath = path.resolve(pkgDir, pkg.types)
+          const existing = await fs.readFile(dtsPath, 'utf-8')
+          const typeFiles = await fs.readdir(typesDir)
+          const toAdd = await Promise.all(
+            typeFiles.map(file =>
+              fs.readFile(path.resolve(typesDir, file), 'utf-8')
+            )
+          )
+          await fs.writeFile(dtsPath, existing + '\n' + toAdd.join('\n'))
+        }
+        console.log(pc.bold(pc.green(`API Extractor completed successfully.`)))
+      } else {
+        console.error(
+          `API Extractor completed with ${extractorResult.errorCount} errors` +
+          ` and ${extractorResult.warningCount} warnings`
+        )
+        process.exitCode = 1
+      }
+
+      if (['vue-i18n', 'petite-vue-i18n'].includes(target)) {
+        console.log()
+        console.log(
+          pc.bold(pc.yellow(`Appending Vue type definitions for ${target}...`))
+        )
+
+        let content = ''
+
+        try {
+          content = await fs.readFile(
+            path.resolve(pkgDir, 'src/vue.d.ts'),
+            'utf-8'
+          )
+        } catch (e) {
+          console.error(
+            `Failed in opening Vue type definition file with error code: ${(e as NodeJS.ErrnoException).code}`
+          )
+          process.exitCode = 1
+        }
+
+        try {
+          const marker =
+            '// --- THE CONTENT BELOW THIS LINE WILL BE APPENDED TO DTS FILE IN DIST DIRECTORY --- //'
+          const data = content.slice(content.indexOf(marker) + marker.length)
+
+          await fs.appendFile(path.resolve(pkgDir, `dist/${target}.d.ts`), data)
+        } catch (e) {
+          console.error('Failed in appending Vue type definitions', e)
+          process.exitCode = 1
+        }
+
+        console.log(
+          pc.bold(
+            pc.green(`Appending Vue type definitions completed successfully.`)
+          )
+        )
+      }
+
+      await fs.rm(`${pkgDir}/dist/packages`, { recursive: true })
+    }
+    */
+  }
+
+  async function checkAllSizes(targets: string[]) {
+    if (devOnly) {
+      return
+    }
+    console.log()
+    for (const target of targets) {
+      await checkSize(target)
+    }
+    console.log()
+  }
+
+  async function checkSize(target: string) {
+    const pkgDir = path.resolve(`packages/${target}`)
+    const files = await checkSizeDistFiles(pkgDir)
+    for (const file of files) {
+      await checkFileSize(`${pkgDir}/dist/${file}`)
+    }
+  }
+
+  async function checkFileSize(filePath: string) {
+    if (!existsSync(filePath)) {
+      return
+    }
+    const file = await fs.readFile(filePath)
+    const filename = path.basename(filePath)
+
+    const gzipped = gzipSync(file)
+    const brotli = brotliCompressSync(file)
+    console.log(
+      `📦  ${pc.green(
+        pc.bold(path.basename(filePath))
+      )} - min: ${displaySize(file.length)} / gzip: ${displaySize(gzipped.length)} / brotli: ${displaySize(brotli.length)}`
+    )
+
+    if (size) {
+      const sizeContents = JSON.stringify(
+        {
+          file: filename,
+          size: file.length,
+          gzip: gzipped.length,
+          brotli: brotli.length
+        },
+        null,
+        2
+      )
+      await fs.writeFile(
+        path.resolve(sizeDir, `${filename}.json`),
+        sizeContents,
+        'utf-8'
+      )
+    }
+  }
+}
+
+main().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
---
+++ scripts/rolldown.ts
@@ -0,0 +1,372 @@
+import polyfillNode from '@rolldown/plugin-node-polyfills'
+import { minify as minifySwc } from '@swc/core'
+import { createRequire } from 'node:module'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import pc from 'picocolors'
+import { entries } from './aliases'
+// @ts-expect-error -- experimental
+import { replacePlugin } from 'rolldown/experimental'
+
+import type { OutputOptions, Plugin, RolldownOptions } from 'rolldown'
+
+const require = createRequire(import.meta.url)
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+const masterVersion = require('../package.json').version
+const packagesDir = path.resolve(__dirname, '../packages')
+
+export function createConfigsForPackage({
+  target,
+  commit,
+  formats,
+  devOnly = false,
+  prodOnly = false,
+  sourceMap = false
+  // localDev = false,
+  // inlineDeps = false
+}: {
+  target: string
+  commit: string
+  formats?: string[]
+  devOnly?: boolean
+  prodOnly?: boolean
+  sourceMap?: boolean
+  localDev?: boolean
+  inlineDeps?: boolean
+}) {
+  const packageDir = path.resolve(packagesDir, target)
+  const resolve = (p: string) => path.resolve(packageDir, p)
+  const pkg = require(resolve(`package.json`))
+  const packageOptions = (pkg.buildOptions || {}) as {
+    name: string
+    formats?: string[]
+    prod?: boolean
+    enableNonBrowserBranches?: boolean
+  }
+  const name = path.basename(packageDir)
+
+  const banner = `/*!
+* ${name} v${pkg.version}
+* (c) 2016-present ${pkg.author.name} and contributors
+* Released under the ${pkg.license} License.
+*/`
+
+  // TODO:
+  /*
+  const stubs = {
+    [`dist/${name}.cjs`]: `${name}.cjs.js`,
+    [`dist/${name}.mjs`]: `${name}.esm-bundler.js`,
+    [`dist/${name}.runtime.mjs`]: `${name}.runtime.esm-bundler.js`,
+    [`dist/${name}.prod.cjs`]: `${name}.cjs.prod.js`
+  }
+  */
+
+  const outputConfigs: Record<string, OutputOptions> = {
+    mjs: {
+      file: `dist/${name}.mjs`,
+      format: `es`
+    },
+    'mjs-node': {
+      file: `dist/${name}.node.mjs`,
+      format: `es`
+    },
+    browser: {
+      file: `dist/${name}.esm-browser.js`,
+      format: `es`
+    },
+    cjs: {
+      // file: `dist/${name}.cjs.js`,
+      file: `dist/${name}.cjs`,
+      format: `cjs`
+    },
+    global: {
+      file: `dist/${name}.global.js`,
+      format: `iife`
+    },
+    // runtime-only builds, for '@intlify/core' and 'vue-i18n' package only
+    'mjs-runtime': {
+      file: `dist/${name}.runtime.mjs`,
+      format: `es`
+    },
+    'mjs-node-runtime': {
+      file: `dist/${name}.runtime.node.mjs`,
+      format: `es`
+    },
+    'browser-runtime': {
+      file: `dist/${name}.runtime.esm-browser.js`,
+      format: 'es'
+    },
+    'global-runtime': {
+      file: `dist/${name}.runtime.global.js`,
+      format: 'iife'
+    }
+  }
+
+  const resolvedFormats = (
+    formats ||
+    packageOptions.formats || ['esm-bundler', 'cjs']
+  )
+    .filter(Boolean)
+    .filter((format: string) => outputConfigs[format])
+
+  const packageConfigs = prodOnly
+    ? []
+    : resolvedFormats.map((format: string) =>
+        createConfig(format, outputConfigs[format])
+      )
+
+  if (!devOnly) {
+    resolvedFormats.forEach((format: string) => {
+      if (packageOptions.prod === false) {
+        return
+      }
+      if (format === 'cjs') {
+        packageConfigs.push(createProductionConfig(format))
+      }
+      if (/^(global|browser)(-runtime)?/.test(format)) {
+        packageConfigs.push(createMinifiedConfig(format))
+      }
+    })
+  }
+
+  function createConfig(
+    format: string,
+    output: OutputOptions,
+    plugins: Plugin[] = []
+  ): RolldownOptions {
+    if (!output) {
+      console.log(pc.yellow(`invalid format: "${format}"`))
+      process.exit(1)
+    }
+
+    const isProductionBuild =
+      process.env.__DEV__ === 'false' ||
+      /\.prod\.[cm]?js$/.test(String(output.file) || '')
+    const isBundlerESMBuild = /mjs/.test(format)
+    const isBrowserESMBuild = /browser/.test(format)
+    const isNodeBuild =
+      String(output.file).includes('.node.') ||
+      format === 'cjs' ||
+      format === 'cjs-lite'
+    const isGlobalBuild = /global/.test(format)
+    const isRuntimeOnlyBuild = /runtime/.test(format)
+    const isLite = /petite-vue-i18n/.test(name)
+
+    output.dir = resolve('dist')
+    output.sourcemap = sourceMap
+    output.banner = banner
+    output.externalLiveBindings = false
+    if (
+      name === 'vue-i18n' ||
+      name === 'vue-i18n-core' ||
+      name === 'petite-vue-i18n'
+    ) {
+      output.globals = {
+        vue: 'Vue',
+        '@vue/devtools-api': 'VueDevtoolsApi'
+      }
+    }
+    if (isGlobalBuild) {
+      output.name = packageOptions.name
+    }
+
+    const entryFile = /runtime/.test(format) ? `src/runtime.ts` : `src/index.ts`
+
+    function resolveDefine() {
+      const defines: Record<string, string> = {
+        __COMMIT__: `"${commit}"`,
+        __VERSION__: `'${masterVersion}'`,
+        /*
+        __DEV__:
+          ['vue-i18n', 'petite-vue-i18n'].includes(name) && isNodeBuild
+            ? 'false' // tree-shake devtools
+            : isBundlerESMBuild
+              ? // preserve to be handled by bundlers
+              `(process.env.NODE_ENV !== 'production')`
+              : // hard coded dev/prod builds
+              !isProductionBuild,
+              */
+        // this is only used during Vue's internal tests
+        __TEST__: `false`,
+        // If the build is expected to run directly in the browser (global / esm builds)
+        __BROWSER__: String(isBrowserESMBuild),
+        __GLOBAL__: String(isGlobalBuild),
+        // for runtime only
+        __RUNTIME__: String(isRuntimeOnlyBuild),
+        // bundle filename
+        // __BUNDLE_FILENAME__: `'${path.parse(String(output.file || '')).base || ''}'`,
+        __ESM_BUNDLER__: String(isBundlerESMBuild),
+        __ESM_BROWSER__: String(isBrowserESMBuild),
+        // is targeting Node (SSR)?
+        __NODE_JS__: String(isNodeBuild),
+        // for lite version
+        __LITE__: String(isLite),
+        // feature flags
+        __FEATURE_FULL_INSTALL__: isBundlerESMBuild
+          ? `__VUE_I18N_FULL_INSTALL__`
+          : `true`,
+        __FEATURE_LEGACY_API__: isBundlerESMBuild
+          ? `__VUE_I18N_LEGACY_API__`
+          : `true`,
+        __FEATURE_PROD_VUE_DEVTOOLS__:
+          ['vue-i18n', 'petite-vue-i18n'].includes(name) && isNodeBuild
+            ? 'false' // tree-shake devtools
+            : isBundlerESMBuild
+              ? `__VUE_PROD_DEVTOOLS__`
+              : `false`,
+        __FEATURE_PROD_INTLIFY_DEVTOOLS__: isBundlerESMBuild
+          ? `__INTLIFY_PROD_DEVTOOLS__`
+          : `false`,
+        __FEATURE_DROP_MESSAGE_COMPILER__: isBundlerESMBuild
+          ? `__INTLIFY_DROP_MESSAGE_COMPILER__`
+          : `false`
+        // ...(isProductionBuild && isBrowserESMBuild
+        //   ? {
+        //     'emitError(': `/*#__PURE__*/ emitError(`,
+        //     'createCompileError(': `/*#__PURE__*/ createCompileError(`,
+        //     'throw createCoreError(': `throw Error(`,
+        //     'throw createI18nError(': `throw Error(`
+        //   }
+        //   : {})
+      }
+
+      if (!isBundlerESMBuild) {
+        // hard coded dev/prod builds
+        defines.__DEV__ = String(!isProductionBuild)
+      }
+
+      // allow inline overrides like
+      //__LITE__=true pnpm build core-base
+      Object.keys(defines).forEach(key => {
+        if (key in process.env) {
+          const value = process.env[key]
+          assert(typeof value === 'string')
+          defines[key] = value
+        }
+      })
+
+      return defines
+    }
+
+    function resolveReplace() {
+      const replacements: Record<string, string> = {}
+
+      if (isBundlerESMBuild) {
+        Object.assign(replacements, {
+          // preserve to be handled by bundlers
+          __DEV__:
+            ['vue-i18n', 'petite-vue-i18n'].includes(name) && isNodeBuild
+              ? 'false' // tree-shake devtools
+              : isBundlerESMBuild
+                ? // preserve to be handled by bundlers
+                  `(process.env.NODE_ENV !== 'production')`
+                : // hard coded dev/prod builds
+                  !isProductionBuild
+        })
+      }
+
+      if (isProductionBuild && isBrowserESMBuild) {
+        Object.assign(replacements, {
+          'emitError(': `/*#__PURE__*/ emitError(`,
+          'createCompileError(': `/*#__PURE__*/ createCompileError(`,
+          'throw createCoreError(': `throw Error(`,
+          'throw createI18nError(': `throw Error(`
+        })
+      }
+
+      if (Object.keys(replacements).length) {
+        return [replacePlugin(replacements)]
+      } else {
+        return []
+      }
+    }
+
+    function resolveExternal() {
+      return isGlobalBuild || isBrowserESMBuild
+        ? ['vue'] // packageOptions.enableNonBrowserBranches
+        : // ? packageOptions.enableFullBundleForEsmBrowser && isBrowserESMBuild
+          //   ? []
+          //   : ['vue'] // packageOptions.enableNonBrowserBranches
+          // Node / esm-bundler builds. Externalize everything.
+          [
+            ...Object.keys(pkg.dependencies || {}),
+            ...Object.keys(pkg.peerDependencies || {})
+          ]
+    }
+
+    function resolveNodePlugins() {
+      const nodePlugins =
+        (format === 'cjs' && Object.keys(pkg.devDependencies || {}).length) ||
+        packageOptions.enableNonBrowserBranches
+          ? [...(format === 'cjs' ? [] : [polyfillNode()])]
+          : []
+      return nodePlugins
+    }
+
+    return {
+      input: resolve(entryFile),
+      // Global and Browser ESM builds inlines everything so that they can be
+      // used alone.
+      external: resolveExternal(),
+      define: resolveDefine(),
+      platform: format === 'cjs' ? 'node' : 'browser',
+      resolve: {
+        alias: entries
+      },
+      plugins: [
+        ...resolveReplace(),
+        ...resolveNodePlugins(),
+        ...plugins
+        // TODO: stub
+      ],
+      output,
+      treeshake: {
+        // https://github.com/rolldown/rolldown/issues/1917
+        moduleSideEffects: false
+      }
+    }
+  }
+
+  function createProductionConfig(format: string): RolldownOptions {
+    const extension = format === 'cjs' || format === 'mjs' ? format : 'js'
+    const descriptor = format === 'cjs' || format === 'mjs' ? '' : `.${format}`
+    return createConfig(format, {
+      file: `dist/${name}${descriptor}.prod.${extension}`,
+      format: outputConfigs[format].format
+    })
+  }
+
+  function createMinifiedConfig(format: string): RolldownOptions {
+    return createConfig(
+      format,
+      {
+        file: String(outputConfigs[format].file).replace(/\.js$/, '.prod.js'),
+        format: outputConfigs[format].format
+      },
+      [
+        {
+          name: 'swc-minify',
+          async renderChunk(contents, _, { format }) {
+            const { code } = await minifySwc(contents, {
+              module: format === 'es',
+              format: {
+                comments: false
+              },
+              compress: {
+                ecma: 2016,
+                pure_getters: true
+              },
+              safari10: true,
+              mangle: true
+            })
+            // swc removes banner
+            return { code: banner + code, map: null }
+          }
+        }
+      ]
+    )
+  }
+
+  return packageConfigs
+}
