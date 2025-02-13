af2a1d0c9d597a11d31005496f0f198104d298b4
babu-ch <mashirohira@gmail.com>
2025-02-13 19:29:09 +0900

    fix others

--- examples/type-safe/type-annotation/vite.config.ts
+++ examples/type-safe/type-annotation/vite.config.ts
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   define: {
-    __VUE_I18N_FULL_INSTALL__: true,
-    __VUE_I18N_LEGACY_API__: true
+    __VUE_I18N_FULL_INSTALL__: true
   }
 })
--- packages/vue-i18n-core/src/warnings.ts
+++ packages/vue-i18n-core/src/warnings.ts
@@ -4,15 +4,7 @@ import { format } from '@intlify/shared'
 export const I18nWarnCodes = {
   FALLBACK_TO_ROOT: CORE_WARN_CODES_EXTEND_POINT as number, // 8
   NOT_FOUND_PARENT_SCOPE: 9,
-  IGNORE_OBJ_FLATTEN: 10,
-  /**
-   * @deprecated will be removed at vue-i18n v12
-   */
-  DEPRECATE_LEGACY_MODE: 11,
-  /**
-   * @deprecated will be removed at vue-i18n v12
-   */
-  DEPRECATE_TRANSLATE_CUSTOME_DIRECTIVE: 12
+  IGNORE_OBJ_FLATTEN: 10
 } as const
 
 type I18nWarnCodes = (typeof I18nWarnCodes)[keyof typeof I18nWarnCodes]
@@ -20,15 +12,7 @@ type I18nWarnCodes = (typeof I18nWarnCodes)[keyof typeof I18nWarnCodes]
 export const warnMessages: { [code: number]: string } = {
   [I18nWarnCodes.FALLBACK_TO_ROOT]: `Fall back to {type} '{key}' with root locale.`,
   [I18nWarnCodes.NOT_FOUND_PARENT_SCOPE]: `Not found parent scope. use the global scope.`,
-  [I18nWarnCodes.IGNORE_OBJ_FLATTEN]: `Ignore object flatten: '{key}' key has an string value`,
-  /**
-   * @deprecated will be removed at vue-i18n v12
-   */
-  [I18nWarnCodes.DEPRECATE_LEGACY_MODE]: `Legacy API mode has been deprecated in v11. Use Composition API mode instead.\nAbout how to use the Composition API mode, see https://vue-i18n.intlify.dev/guide/advanced/composition.html`,
-  /**
-   * @deprecated will be removed at vue-i18n v12
-   */
-  [I18nWarnCodes.DEPRECATE_TRANSLATE_CUSTOME_DIRECTIVE]: `'v-t' has been deprecated in v11. Use translate APIs ('t' or '$t') instead.`
+  [I18nWarnCodes.IGNORE_OBJ_FLATTEN]: `Ignore object flatten: '{key}' key has an string value`
 }
 
 export function getWarnMessage(
