e6967f6f70ede87c3a17ad2f1417154c6c80263c
kazuya kawaguchi <kawakazu80@gmail.com>
2025-10-22 16:33:08 +0900

    chore: fix oxc-transform errors

--- packages/core-base/src/ast.ts
+++ packages/core-base/src/ast.ts
@@ -19,25 +19,25 @@ export function isMessageAST(val: unknown): val is ResourceNode {
 
 const PROPS_BODY = ['b', 'body']
 
-export function resolveBody(node: ResourceNode) {
+export function resolveBody(node: ResourceNode): ReturnType<typeof resolveProps> {
   return resolveProps<MessageNode | PluralNode>(node, PROPS_BODY)
 }
 
 const PROPS_CASES = ['c', 'cases']
 
-export function resolveCases(node: PluralNode) {
+export function resolveCases(node: PluralNode): ReturnType<typeof resolveProps> {
   return resolveProps<PluralNode['cases'], PluralNode['cases']>(node, PROPS_CASES, [])
 }
 
 const PROPS_STATIC = ['s', 'static']
 
-export function resolveStatic(node: MessageNode) {
+export function resolveStatic(node: MessageNode): ReturnType<typeof resolveProps> {
   return resolveProps(node, PROPS_STATIC)
 }
 
 const PROPS_ITEMS = ['i', 'items']
 
-export function resolveItems(node: MessageNode) {
+export function resolveItems(node: MessageNode): ReturnType<typeof resolveProps> {
   return resolveProps<MessageNode['items'], MessageNode['items']>(node, PROPS_ITEMS, [])
 }
 
@@ -63,13 +63,13 @@ export function resolveValue<Message = string>(
 
 const PROPS_MODIFIER = ['m', 'modifier']
 
-export function resolveLinkedModifier(node: LinkedNode) {
+export function resolveLinkedModifier(node: LinkedNode): ReturnType<typeof resolveProps> {
   return resolveProps<LinkedModifierNode>(node, PROPS_MODIFIER)
 }
 
 const PROPS_KEY = ['k', 'key']
 
-export function resolveLinkedKey(node: LinkedNode) {
+export function resolveLinkedKey(node: LinkedNode): ReturnType<typeof resolveProps> {
   const resolved = resolveProps<LinkedNode['key']>(node, PROPS_KEY)
   if (resolved) {
     return resolved
@@ -94,7 +94,7 @@ export function resolveProps<T = string, Default = undefined>(
   return defaultValue as Default
 }
 
-export const AST_NODE_PROPS_KEYS = [
+export const AST_NODE_PROPS_KEYS: string[] = [
   ...PROPS_BODY,
   ...PROPS_CASES,
   ...PROPS_STATIC,
@@ -105,6 +105,6 @@ export const AST_NODE_PROPS_KEYS = [
   ...PROPS_TYPE
 ]
 
-export function createUnhandleNodeError(type: NodeTypes) {
+export function createUnhandleNodeError(type: NodeTypes): Error {
   return new Error(`unhandled node type: ${type}`)
 }
