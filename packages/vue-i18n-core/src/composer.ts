f55c356a59aef1634d9a454687cef581fbb2a36c
kazuya kawaguchi <kawakazu80@gmail.com>
2025-10-23 17:28:28 +0900

    feat: support Vue3 vapor mode

--- packages/vue-i18n-core/src/composer.ts
+++ packages/vue-i18n-core/src/composer.ts
@@ -37,7 +37,7 @@ import {
   isString,
   warn
 } from '@intlify/shared'
-import { computed, getCurrentInstance, ref, shallowRef, watch } from 'vue'
+import { computed, ref, shallowRef, watch } from 'vue'
 import { I18nErrorCodes, createI18nError } from './errors'
 import { VERSION } from './misc'
 import {
@@ -49,7 +49,13 @@ import {
   SetPluralRulesSymbol,
   TranslateVNodeSymbol
 } from './symbols'
-import { createTextNode, getComponentOptions, getLocaleMessages, handleFlatJson } from './utils'
+import {
+  createTextNode,
+  getComponentOptions,
+  getCurrentInstance,
+  getLocaleMessages,
+  handleFlatJson
+} from './utils'
 import { I18nWarnCodes, getWarnMessage } from './warnings'
 
 import type {
@@ -104,6 +110,7 @@ import type { VueDevToolsEmitter } from '@intlify/devtools-types'
 import type {
   ComponentInternalInstance,
   ComputedRef,
+  GenericComponentInstance,
   VNode,
   VNodeArrayChildren,
   WritableComputedRef
@@ -221,7 +228,7 @@ export type DefaultNumberFormatSchema<
 export type MissingHandler = (
   locale: Locale,
   key: Path,
-  instance?: ComponentInternalInstance,
+  instance?: ComponentInternalInstance | GenericComponentInstance,
   type?: string
 ) => string | void
 
