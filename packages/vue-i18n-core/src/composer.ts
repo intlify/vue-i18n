e75d3c3afbc05551b50dbbf91e4c9ecd2a006c77
Dr Alex Mitre <bedr10_capacitacion@hotmail.com>
2026-03-05 10:23:02 -0600

    docs: document possible errors for datetime/number formatting APIs (issue #1749)

--- packages/vue-i18n-core/src/composer.ts
+++ packages/vue-i18n-core/src/composer.ts
@@ -1073,6 +1073,8 @@ export interface ComposerDateTimeFormatting<
    * See about:
    * - [Datetime formatting](../../../guide/essentials/datetime)
    *
+   * @throws Error if `value` is not a valid date value.
+   *
    * @param value - A value, timestamp number or `Date` instance or ISO 8601 string
    *
    * @returns Formatted value
@@ -1169,6 +1171,8 @@ export interface ComposerNumberFormatting<
    *
    * See about:
    * - [Number formatting](../../../guide/essentials/number)
+   *
+   * @throws Error if `value` is not a number.
    *
    * @param value - A number value
    *
